264fbde9c4aeb75fbca235de19f2390481868be6
Hesam <hesoyam.h.0913@gmail.com>
2025-02-10 15:01:42 +0330

    Add Login Register Errors to Views

--- TStore/Controllers/AccountController.cs
+++ TStore/Controllers/AccountController.cs
@@ -4,55 +4,46 @@ using TStore.Data;
 
 namespace TStore.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : Controller
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-
-        public IActionResult Register()
-        {
-            return View();
-        }
+        public IActionResult Register() => View();
 
         [HttpPost]
         public async Task<IActionResult> Register(string username, string email, string password, string firstName, string lastName)
         {
             var user = new ApplicationUser { UserName = username, Email = email, FirstName = firstName, LastName = lastName };
-            var result = await _userManager.CreateAsync(user, password);
+            var result = await userManager.CreateAsync(user, password);
 
             if (result.Succeeded)
             {
-                await _signInManager.SignInAsync(user, isPersistent: false);
+                await signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction("Index", "Home");
             }
 
-            return BadRequest(result.Errors);
-        }
-
-        public IActionResult Login()
-        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description); 
+            }
             return View();
         }
+
+        public IActionResult Login()=> View();
+
         [HttpPost]
         public async Task<IActionResult> Login(string username, string password, bool rememberMe)
         {
-            var result = await _signInManager.PasswordSignInAsync(username, password, rememberMe, lockoutOnFailure: false);
+            var result = await signInManager.PasswordSignInAsync(username, password, rememberMe, lockoutOnFailure: false);
 
             if (result.Succeeded)
                 return RedirectToAction("Index", "Home");
-
-            return Unauthorized("نام کاربری یا رمز عبور اشتباه است");
+            
+            ModelState.AddModelError(string.Empty, "The username or password is incorrect."); 
+            return View();
         }
 
         public async Task<IActionResult> Logout()
         {
-            await _signInManager.SignOutAsync();
+            await signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }

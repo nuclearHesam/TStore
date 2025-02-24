a6bca512a0a7bda8aaf1dd192b3414f34843da44
Hesam <hesoyam.h.0913@gmail.com>
2025-02-24 18:18:13 +0330

    Add Product Model and realations to db, Add product Views and Controller

--- TStore/Areas/Admin/Controllers/ProductController.cs
+++ TStore/Areas/Admin/Controllers/ProductController.cs
@@ -12,11 +12,11 @@ namespace TStore.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             ViewBag.Categories = await context.Categories.ToArrayAsync();
-            return View( context.Products.Include(p => p.Category).AsNoTracking());
+            return View(await context.Products.Include(p => p.Category).AsNoTracking().ToListAsync());
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(string Name,decimal Price,string Brand,IFormFile FormImage,string? Description,string CategoryId)
+        public async Task<IActionResult> Create(string Name, decimal Price, string Brand, IFormFile FormImage, string? Description, string CategoryId)
         {
             string imageData = "";
             if (FormImage != null && FormImage.Length != 0)
@@ -66,6 +66,52 @@ namespace TStore.Areas.Admin.Controllers
             }
 
             return Json(new { success = true, message = "Product deleted successfully!" });
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            ViewBag.Categories = await context.Categories.ToArrayAsync();
+            return View(await context.Products.FirstOrDefaultAsync(p => p.ProductId == id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string ProductId, string Name, int Price,string Brand, IFormFile? FormImage, string Description,string CategoryId)
+        {
+            var product = await context.Products.FindAsync(ProductId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Name = Name;
+            product.Brand = Brand;
+            product.Price = Price;
+            product.Description = Description;
+            product.CategoryId = CategoryId;
+
+            string imageData = "";
+            if (FormImage != null && FormImage.Length != 0)
+            {
+                string path = webHostEnvironment.WebRootPath + product.Image;
+                System.IO.File.Delete(path);
+
+                string UploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                Directory.CreateDirectory(UploadsFolder);
+
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetExtension(FormImage.FileName);
+                string filePath = Path.Combine(UploadsFolder, uniqueFileName);
+
+                using FileStream fileStream = new(filePath, FileMode.Create);
+                await FormImage.CopyToAsync(fileStream);
+
+                imageData = "/images/" + uniqueFileName;
+
+                product.Image = imageData;
+            }
+
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }

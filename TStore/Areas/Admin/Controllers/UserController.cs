a4087618fabfd71151dc1f04d619d3add40e4052
Hesam <hesoyam.h.0913@gmail.com>
2025-02-24 21:26:34 +0330

    Add User Management view and Controller

---
+++ TStore/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using System.Formats.Asn1;
+using TStore.Data;
+
+namespace TStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserController(UserManager<ApplicationUser> userManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            var userViewModels = users.Select(u => new UserViewModel
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Email = u.Email,
+                UserName = u.UserName,
+                Password = u.PasswordHash
+            });
+
+            return View(userViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string FirstName,string LastName,string Email,string UserName,string Password)
+        {
+            var user = new ApplicationUser {FirstName = FirstName,LastName= LastName, Email =Email,UserName = UserName };
+            await userManager.CreateAsync(user, Password);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+            
+            if(user == null)
+            {
+                return NotFound();
+            }
+            
+            await userManager.DeleteAsync(user);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
